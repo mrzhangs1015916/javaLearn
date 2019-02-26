0554477623c0bffff68b66aea6ad08e1b1763512
zhangshuai <807167807@qq.com>
2019-02-26 23:02:46 +0800

    second

--- src/mapAndSet/ListTest.java
+++ src/mapAndSet/ListTest.java
@@ -1,4 +1,21 @@
 package mapAndSet;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class ListTest {
+    public static void main(String [] args){
+        System.out.println("ArrayList");
+        List<String> str = new ArrayList<String>();
+        str.add("admin");
+        str.add("test");
+        str.add(1,"insert");
+        System.out.println(str);
+        System.out.println(str.contains("admin"));
+        System.out.println(str.indexOf("test"));
+        System.out.println(str.get(1));
+        System.out.println(str.size());
+        str.remove(0);
+        System.out.println(str);
+    }
 }
